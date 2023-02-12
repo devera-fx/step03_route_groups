dc69f9a36d9396fe2d418742d65539260e43a087
devera-fx <aakifnajib@gmail.com>
2023-02-12 23:26:51 +0500

    commit changes 1

---
+++ src/app/(personal)/name/address/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function GiveAddress() {
+    return (
+      <div>
+            <h2>I live in Lahore, Pakistan</h2>
+            <br/>
+          <Link href="/">Home</Link>
+      </div>
+    )
+  }
